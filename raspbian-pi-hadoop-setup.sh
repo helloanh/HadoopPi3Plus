772f3e261dbb56eaf3425f872224631d35b53d38
Anh K <ahoang10@jhu.edu>
2019-05-02 19:55:31 -0400

    Update raspbian-pi-hadoop-setup.sh

--- raspbian-pi-hadoop-setup.sh
+++ raspbian-pi-hadoop-setup.sh
@@ -1,7 +1,16 @@
+# Bash script to automate the installation process
+# raspbian-pi-hadoop-setup.sh
+
+# This is the directory to store our hadoop instance per node, all master and slave nodes need this.
 mkdir /opt/hadoop/
 mkdir /opt/hadoop_tmp/
+
+# Setting up java.  We need Java 8
 apt-get update
 apt-get install oracle-java8-jdk
+
+# Making sure we have ssh installed
 apt-get remove openssh-client openssh-server
 apt-get remove openssh-client openssh-server
+
 sudo apt-get install ssh
