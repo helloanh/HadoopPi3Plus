1937b85899e64375efc423d2faf0a367edf21702
Anh K <ahoang10@jhu.edu>
2019-05-02 11:28:42 -0400

    Update raspbian-pi-hadoop-setup.sh

--- raspbian-pi-hadoop-setup.sh
+++ raspbian-pi-hadoop-setup.sh
@@ -2,4 +2,6 @@ mkdir /opt/hadoop/
 mkdir /opt/hadoop_tmp/
 apt-get update
 apt-get install oracle-java8-jdk
-
+apt-get remove openssh-client openssh-server
+apt-get remove openssh-client openssh-server
+sudo apt-get install ssh
